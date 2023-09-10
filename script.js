88fbb96b70b255eab41794e1d1fae75bcc1750ea
kathatbat <katoria1234@icloud.com>
2023-09-10 17:39:42 -0500

    update Javascript

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+function generatePassword() {
+  var uppercase= confirm("Do you want your password to include uppercase letters?");
+  var lowercase= confirm("Do you want your password to have lowecase letters?");
+  var specialcharacters= confirm("Do you want your password to have special characters?");
+  var numbers= confirm("Do you want your password to have numbers?");
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
