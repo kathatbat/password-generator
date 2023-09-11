f2f1621b41563fc7d77d5a20cf6f0160b3c31a9e
kathatbat <katoria1234@icloud.com>
2023-09-11 18:26:07 -0500

    update challenge

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-function generatePassword() {
-  var uppercase= confirm("Do you want your password to include uppercase letters?");
-  var lowercase= confirm("Do you want your password to have lowecase letters?");
-  var specialcharacters= confirm("Do you want your password to have special characters?");
-  var numbers= confirm("Do you want your password to have numbers?");
+
+function generatePassword(){
+  //prompt user to specify length of password
+  let length= prompt("Length of password?");
+  if (length < 8 || length > 128) {
+    alert("Your password must be 8-128 characters.");
+  }
+  //confirm if lowercase
+  var lowercase= confirm("Include lowercase characters?");
+  //confirm if uppercase
+  var uppercase= confirm("Include uppercase?");
+  //confirm if numbers
+ var numbers= confirm("Include numbers?");
+  //confirm if special characters
+  var special= confirm("Include special characters?");
+  if (lowercase === false && uppercase === false && numbers === false && special === false) {
+    return "Your password does not meet the criteria."
+  }
+  //return password
+var numeric = "0123456789";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialC = "!@#$%^&*()?.<>/|=+:;,[-_]";
+var MultiS = [];
+  if (numbers) {MultiS += numeric};
+  if (lowercase) {MultiS += lowerCase};
+  if (uppercase) {MultiS += upperCase};
+  if (special) {MultiS += specialC};
+
+var password = ""
+  for (let i = 0; i < length; i++) {
+    var randomNumber = Math.floor(Math.random() * MultiS.length);
+    password = password + MultiS[randomNumber];
+  }
+  return password;
 }
 // Write password to the #password input
 function writePassword() {
